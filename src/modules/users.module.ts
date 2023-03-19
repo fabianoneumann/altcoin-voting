ba1705d582152e8320bbc2302ef96e2af79c0536
Fabiano Neumann <fabianoneumann@gmail.com>
2023-03-19 02:23:41 -0300

    Criação de Altcoins, Usuários e Votos

---
+++ src/modules/users.module.ts
@@ -0,0 +1,19 @@
+/* eslint-disable prettier/prettier */
+import { Module } from '@nestjs/common';
+import { PrismaService } from 'src/database/prisma.service';
+import { UsersController } from 'src/controllers/users.controllers';
+import { UsersRepository } from 'src/repositories/users-repository';
+import { PrismaUsersRepository } from 'src/repositories/prisma/prisma-users-repository';
+
+@Module({
+  imports: [],
+  controllers: [UsersController],
+  providers: [
+    PrismaService,
+    {
+      provide: UsersRepository,
+      useClass: PrismaUsersRepository,
+    },
+  ],
+})
+export class UsersModule {}
---
+++ src/repositories/prisma/prisma-votes-repository.ts
@@ -0,0 +1,27 @@
+/* eslint-disable prettier/prettier */
+import { Injectable } from '@nestjs/common';
+import { Vote } from '@prisma/client';
+import { randomUUID } from 'node:crypto';
+import { PrismaService } from 'src/database/prisma.service';
+import { VotesRepository } from '../votes-repository';
+
+@Injectable()
+export class PrismaVotesRepository implements VotesRepository {
+  constructor(private prisma: PrismaService) {}
+
+  async create(altcoinId: string, userId: string): Promise<void> {
+    await this.prisma.vote.create({
+      data: {
+        id: randomUUID(),
+        altcoinId,
+        userId,
+      },
+    });
+  }
+
+  async findMany(): Promise<Vote[]> {
+    const votes = await this.prisma.vote.findMany();
+
+    return votes;
+  }
+}
